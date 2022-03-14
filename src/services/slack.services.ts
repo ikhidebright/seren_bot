216113c3e34e69b22c0060f9f2754fbf1f28b0dc
bright.ikhide <bright.ikhide@e4email.net>
2022-03-14 02:35:21 +0100

    changed slack envs

--- src/services/slack.services.ts
+++ src/services/slack.services.ts
@@ -22,15 +22,37 @@ slackApp.command("/bot", async ({ ack, payload, context }: any) => {
             type: "mrkdwn",
             text: "Welcome. How are you doing?",
           },
-          // value: "totototototototototototoototototootototototototototo",
           accessory: {
             action_id: "text1234",
             // NOTE: We can change type to external_select (to fetch options from an api as user types)
-            type: "external_select",
+            type: "static_select",
             placeholder: {
               type: "plain_text",
               text: "Select how you're doing",
             },
+            options: [
+              {
+                text: {
+                  type: "plain_text",
+                  text: "Doing Well",
+                },
+                value: "value-0",
+              },
+              {
+                text: {
+                  type: "plain_text",
+                  text: "Neutral",
+                },
+                value: "value-1",
+              },
+              {
+                text: {
+                  type: "plain_text",
+                  text: "Feeling Lucky",
+                },
+                value: "value-2",
+              },
+            ],
           },
         },
       ],
