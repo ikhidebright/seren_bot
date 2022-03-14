0cfbffe3daec98b903cc2ea3842e551c0794c5f4
bright.ikhide <bright.ikhide@e4email.net>
2022-03-14 02:05:26 +0100

    changed slack envs

--- src/services/slack.services.ts
+++ src/services/slack.services.ts
@@ -30,29 +30,29 @@ slackApp.command("/bot", async ({ ack, payload, context }: any) => {
               type: "plain_text",
               text: "Select how you're doing",
             },
-            options: [
-              {
-                text: {
-                  type: "plain_text",
-                  text: "Doing Well",
-                },
-                value: "value-0",
-              },
-              {
-                text: {
-                  type: "plain_text",
-                  text: "Neutral",
-                },
-                value: "value-1",
-              },
-              {
-                text: {
-                  type: "plain_text",
-                  text: "Feeling Lucky",
-                },
-                value: "value-2",
-              },
-            ],
+            // options: [
+            //   {
+            //     text: {
+            //       type: "plain_text",
+            //       text: "Doing Well",
+            //     },
+            //     value: "value-0",
+            //   },
+            //   {
+            //     text: {
+            //       type: "plain_text",
+            //       text: "Neutral",
+            //     },
+            //     value: "value-1",
+            //   },
+            //   {
+            //     text: {
+            //       type: "plain_text",
+            //       text: "Feeling Lucky",
+            //     },
+            //     value: "value-2",
+            //   },
+            // ],
           },
         },
       ],
